f6a59f9867bcdb9074affa31d5dbbf0e55766414
Vova Stepanenko <vova.stepanenko2020@gmail.com>
2016-11-08 21:55:03 +0200

    Дз 2 модуль 2

--- goJava/modul2/homeWork2/BankOperation.java
+++ goJava/modul2/homeWork2/BankOperation.java
@@ -1,4 +1,6 @@
-package goJava.modul2.homeWork2;import java.io.BufferedReader;
+package goJava.modul2.homeWork2;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Locale;
@@ -12,55 +14,59 @@ public class BankOperation {
     static double withdrawal;
     static double comision;
 
+    static public boolean isValid(String input) {
+
+        for (char ch : input.toCharArray()) {
+
+            if (!Character.isDigit(ch)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
     public static void main(String[] args) throws IOException {
 
 
         System.out.println("Задайте баланс, по условию задачи");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        loopValid1:
+
+        String input;
+
+
         while (true) {
-            boolean i = false;
-
-            try {
-                balance = Integer.parseInt(reader.readLine());
-            } catch (NumberFormatException e) {
-                System.out.println("Введите число");
-                i = true;
-                continue loopValid1;
-
-            } finally {
-                if (i) break;
+            System.out.println("Введите число");
+            input = reader.readLine();
+            if (isValid(input)) {
+                balance = Double.parseDouble(input);
+                break;
             }
         }
-        System.out.println("Сколько бабла снять ?");
 
-        loopValid2:
+        System.out.println("Сколько денег снять ?");
+
         while (true) {
-            boolean i = false;
-
-            try {
-                withdrawal = Double.parseDouble(reader.readLine());
-            } catch (NumberFormatException e) {
-                System.out.println("Введите число");
-                i = true;
-                continue loopValid2;
-
-            } finally {
-                if (i) break;
+            System.out.println("Введите число");
+            input = reader.readLine();
+            if (isValid(input)) {
+                withdrawal = Double.parseDouble(input);
+                break;
             }
         }
+
         withdrawal(withdrawal);
 
     }
 
+
     public static void withdrawal(double withdrawal) {
 
-        comision=withdrawal*0.05;
-        if(balance>=(comision+withdrawal)) {
+        comision = withdrawal * 0.05;
+        if (balance >= (comision + withdrawal)) {
             balance -= (withdrawal + comision);
-            System.out.printf(new Locale("ru"),"OK \n balance = %.2f, comision =%.2f ",balance,comision);
-        }
-        else System.out.println("NO");
+            System.out.printf(new Locale("ru"), "OK \n balance = %.2f, comision =%.2f ", balance, comision);
+        } else System.out.println("NO");
 
     }
 }
