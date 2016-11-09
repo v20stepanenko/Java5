87bb3f40a6e6833ca383e55c60a6b78ad794e24e
Vova Stepanenko <vova.stepanenko2020@gmail.com>
2016-11-09 12:39:36 +0200

    Дз 2 модуль 3

---
+++ goJava/modul3/homeWork2/Adder.java
@@ -0,0 +1,20 @@
+package goJava.modul3.homeWork2;
+
+/**
+ * Created by Vova on 09.11.2016.
+ */
+public class Adder extends Arithmetic {
+
+    static boolean check (Integer  a, Integer  b){
+        return a>b;
+    }
+
+    public static void main(String[] args) {
+
+        Adder intTest = new Adder();
+        System.out.println(intTest.add(5,6));
+        System.out.println(intTest.check(10,7));
+
+    }
+
+}
