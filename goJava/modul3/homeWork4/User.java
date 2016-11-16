7a02ce9743b5b9b8039a3c111b2dd955cf0e8fc5
Vova Stepanenko <vova.stepanenko2020@gmail.com>
2016-11-16 11:01:25 +0200

    Дз 4 модуль 3

---
+++ goJava/modul3/homeWork4/User.java
@@ -0,0 +1,84 @@
+package goJava.modul3.homeWork4;
+
+/**
+ * Created by Vova on 16.11.2016.
+ */
+public class User {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getMonthsOfEmployment() {
+        return monthsOfEmployment;
+    }
+
+    public void setMonthsOfEmployment(int monthsOfEmployment) {
+        this.monthsOfEmployment = monthsOfEmployment;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    private String name;
+    private int balance;
+    private int monthsOfEmployment;
+    private String companyName;
+    private int salary;
+    private String currency;
+
+    void paySalary(int salary) {
+        this.balance += salary;
+    }
+    void withdraw(int withdraw){
+        double comision;
+        if (withdraw<1000){
+            comision = 0.05;
+        }
+        else comision = 0.1;
+
+        this.balance -=(withdraw+withdraw*comision);
+    }
+
+    int companyNameLenght (String companyName){
+        return companyName.length();
+    }
+
+    void  monthIncreaser(int addMonth){
+
+        this.monthsOfEmployment +=addMonth;
+
+    }
+}
