1c1b87a5407c1ef6107877f20caed6797617f212
Vova Stepanenko <vova.stepanenko2020@gmail.com>
2016-12-06 12:04:21 +0200

    add commit

---
+++ goJava/modul6/homeWork/Main.java
@@ -0,0 +1,22 @@
+package goJava.modul6.homeWork;
+
+/**
+ * Created by Vova on 06.12.2016.
+ */
+public class Main {public static void main(String[] args) {
+    User[] users = new User[4];
+    users[0]= new User(1, "Alex", "Black", 3500, 9000);
+    users[1]= new User(2, "Tomas", "White", 3000, 15000);
+    users[2]= new User(3, "Bob", "Green", 3500, 7000);
+    users[3]= new User(1, "Alex", "Black", 3500, 9000);
+
+    UserUtils userUtils = new UserUtils();
+
+    for (User user : userUtils.uniqueUsers(users)){
+        System.out.println(user.getFirstName());
+    }
+    for (User user : userUtils.usersWithConditionalBalance(users, 7000)){
+        System.out.println(user.getFirstName());
+    }
+}
+}
